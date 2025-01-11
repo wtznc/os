2e7c4b9ae8db6dd44d095895e479db5afacdbd9f
Wojciech Tyziniec <10180694+wtznc@users.noreply.github.com>
2025-01-11 22:13:16 +0100

    Add printf implementation and update kernel_main for formatted output

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ CC=/opt/homebrew/opt/llvm/bin/clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
 # Build the kernel
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c
 
 # Start QEMU and load the kernel
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf

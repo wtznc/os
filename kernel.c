b3c336dfe99c2b06084599073e083f93ee103ce9
Wojciech Tyziniec <10180694+wtznc@users.noreply.github.com>
2025-01-11 21:31:27 +0100

    Fix type casting in memset function and update run script to include build commands for the kernel

--- kernel.c
+++ kernel.c
@@ -3,8 +3,9 @@ typedef unsigned int uint32_t;
 typedef uint32_t size_t;
 
 extern char __bss[], __bss_end[], __stack_top[];
+
 void *memset(void *buf, char c, size_t n) {
-    uint8_t *p = (uint8_t) buf;
+    uint8_t *p = (uint8_t *) buf;
     while (n--) {
         *p++ = c;
     }
--- run.sh
+++ run.sh
@@ -4,5 +4,12 @@ set -xue
 # QEMU file path
 QEMU=qemu-system-riscv32
 
-# Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+# Path to clang and compiler flags
+CC=/opt/homebrew/opt/llvm/bin/clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# Build the kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
+
+# Start QEMU and load the kernel
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf

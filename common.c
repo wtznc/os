5b0e7d492770fd66eb88138b3e2c867851159677
Wojciech Tyziniec <10180694+wtznc@users.noreply.github.com>
2025-01-30 00:30:08 +0100

    Implement memory and string manipulation functions: memcpy, memset, strcpy, and strcmp; update common.h and kernel.c

--- common.c
+++ common.c
@@ -57,3 +57,46 @@ void printf(const char *fmt, ...){
     end:
         va_end(vargs);
 }
+// Memory operations
+void *memcpy(void *dst, const void *src, size_t n){
+    uint8_t *d = (uint8_t *) dst;
+    const uint8_t *s = (const uint8_t *) src;
+    while (n--)
+        *d++ = *s++;
+    return dst;
+}
+
+void *memset(void *buf, char c, size_t n){
+    uint8_t *p = (uint8_t *) buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
+
+// String operations
+
+/*
+ The strcpy function continues copying, even if src is longer than the memory area of dst.
+ This can cause a buffer overflow. Do not use it in production code!
+
+ Alternative: strcpy_s
+*/
+char *strcpy(char *dst, const char *src) {
+    char *d = dst;
+    while (*src)
+        *d++ = *src++;
+    *d = '\0';
+    return dst;
+}
+
+int strcmp(const char *s1, const char *s2){
+    while (*s1 && *s2) {
+        if (*s1 != *s2)
+            break;
+        s1++;
+        s2++;
+    }
+
+    // The cast is necessary because the return type is int.
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
--- kernel.c
+++ kernel.c
@@ -29,19 +29,14 @@ void putchar(char ch) {
     sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /* Console Putchar */);
 }
 
-
-
-void *memset(void *buf, char c, size_t n) {
-    uint8_t *p = (uint8_t *) buf;
-    while (n--) {
-        *p++ = c;
-    }
-    return buf;
-}
-
 void kernel_main(void) {
     printf("\n\nHello %s\n", "World");
     printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+
+
+    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+    PANIC("booted!");
+    printf("unreachable here!\n");
 
     for (;;) {
         __asm__ __volatile__("wfi");
